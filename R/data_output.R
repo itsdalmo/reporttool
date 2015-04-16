2ee9ba7cd0af3ae5046370e39ff12319b3bf4f27
Kristian <kristian@doingit.no>
2015-04-16 17:24:41 +0200

    Renamed data_output functions to be more descriptive

--- R/data_output.R
+++ R/data_output.R
@@ -1,3 +1,90 @@
+#' Write data to sheet (in an openxlsx workbook)
+#'
+#' This function (a very thin wrapper for openxlsx functions) that writes/appends 
+#' a data.frame to the specified sheet in a loaded openxlsx workbook.
+#'
+#' @param df The data to write.
+#' @param wb A loaded openxlsx workbook (use openxlsx::loadWorkbook)
+#' @param sheet Name of the sheet to write to, will be created if it does not exist.
+#' @param row Optional: Also specify the startingrow for writing data.
+#' @param append Whether or not the function should append data or clean the
+#' sheet before writing.
+#' @author Kristian D. Olsen
+#' @return A list containing data.frames matching the sheets in the .xlsx file.
+#' If only one sheet is read, the function returns a data.frame instead.
+#' @note This function requires openxlsx.
+#' @import openxlsx
+#' @export
+#' @examples 
+#' wb <- openxlsx::loadWorkbook("test.xlsx")
+#' x %>% to_sheet(wb, sheet = "test", append = FALSE)
+#' openxlsx::saveWorkbook(wb, "test.xlsx", overwrite = TRUE)
+
+to_sheet <- function(df, wb, sheet="analysis", row=1L, append=TRUE) {
+  
+  # Check input
+  if (!is.character(sheet) || length(sheet) != 1L) {
+    stop("The sheet has to be a string of length 1 (not an index).", call. = FALSE)
+  }
+  
+  # See if sheet exists
+  sheet_exists <- sheet %in% openxlsx::sheets(wb)
+  
+  # Get last row if sheet exists, or create if it does not.
+  if (sheet_exists && isTRUE(append)) {
+    row <- 2L + nrow(openxlsx::read.xlsx(wb, sheet = sheet, colNames = FALSE,
+                                         skipEmptyRows = FALSE))
+  } else if (sheet_exists) {
+    openxlsx::removeWorksheet(wb, sheet)
+    openxlsx::addWorksheet(wb, sheetName = sheet)
+  } else {
+    openxlsx::addWorksheet(wb, sheetName = sheet)
+  }
+  
+  # Add data to the workbook
+  if (is.null(names(df)) || identical(names(df), character(0))) {
+    warning(sheet, ": No columnames in data. An empty sheet was created", call. = FALSE)
+  } else {
+    openxlsx::writeData(wb, sheet, df, startRow = row, headerStyle = openxlsx_style)
+  }
+}
+
+#' Write to Windows clipboard
+#'
+#' Wrapper for writing to windows clipboard with the most-used defaults for a
+#' scandinavian locale.
+#'
+#' @param df The data to write.
+#' @param encoding The encoding to use when writing.
+#' @author Kristian D. Olsen
+#' @note This function only works on Windows, and the data-size cannot exceeed 128kb.
+#' @export
+#' @examples 
+#' x %>% to_clipboard()
+
+to_clipboard <- function(df, encoding = "latin1") {
+  
+  if (!identical(Sys.info()["sysname"], "Windows")) {
+    stop("Writing to clipboard requires Windows OS")
+  }
+  
+  if (object.size(df) > 120000) {
+    stop("The data is too large to write to clipboard", call. = FALSE)
+  }
+  
+  if (!inherits(df, "data.frame")) {
+    stop("The data must be a data.frame", call. = FALSE)
+  }
+  
+  write.table(x = df,
+              file = "clipboard-128",
+              sep = "\t",
+              na = "",
+              dec = ",",
+              row.names = FALSE,
+              fileEncoding = encoding)
+}
+
 #' Write common file formats
 #'
 #' A simple wrapper for writing common data formats. The specific format is
@@ -56,93 +143,6 @@ write_data <- function(x, file = NULL, encoding = "latin1") {
   invisible()
 }
 
-#' Write data to an openxlsx workbook
-#'
-#' This function (a very thin wrapper for openxlsx functions) that writes/appends 
-#' a data.frame to the specified sheet in a loaded openxlsx workbook.
-#'
-#' @param df The data to write.
-#' @param wb A loaded openxlsx workbook (use openxlsx::loadWorkbook)
-#' @param sheet Name of the sheet to write to, will be created if it does not exist.
-#' @param row Optional: Also specify the startingrow for writing data.
-#' @param append Whether or not the function should append data or clean the
-#' sheet before writing.
-#' @author Kristian D. Olsen
-#' @return A list containing data.frames matching the sheets in the .xlsx file.
-#' If only one sheet is read, the function returns a data.frame instead.
-#' @note This function requires openxlsx.
-#' @import openxlsx
-#' @export
-#' @examples 
-#' wb <- openxlsx::loadWorkbook("test.xlsx")
-#' x %>% write_sheets(wb, sheet = "test", append = FALSE)
-#' openxlsx::saveWorkbook(wb, "test.xlsx", overwrite = TRUE)
-
-write_sheets <- function(df, wb, sheet="analysis", row=1L, append=TRUE) {
-  
-  # Check input
-  if (!is.character(sheet) || length(sheet) != 1L) {
-    stop("The sheet has to be a string of length 1 (not an index).", call. = FALSE)
-  }
-  
-  # See if sheet exists
-  sheet_exists <- sheet %in% openxlsx::sheets(wb)
-  
-  # Get last row if sheet exists, or create if it does not.
-  if (sheet_exists && isTRUE(append)) {
-    row <- 2L + nrow(openxlsx::read.xlsx(wb, sheet = sheet, colNames = FALSE,
-                                         skipEmptyRows = FALSE))
-  } else if (sheet_exists) {
-    openxlsx::removeWorksheet(wb, sheet)
-    openxlsx::addWorksheet(wb, sheetName = sheet)
-  } else {
-    openxlsx::addWorksheet(wb, sheetName = sheet)
-  }
-  
-  # Add data to the workbook
-  if (is.null(names(df)) || identical(names(df), character(0))) {
-    warning(sheet, ": No columnames in data. An empty sheet was created", call. = FALSE)
-  } else {
-    openxlsx::writeData(wb, sheet, df, startRow = row, headerStyle = openxlsx_style)
-  }
-}
-
-#' Write to Windows clipboard
-#'
-#' Wrapper for writing to windows clipboard with the most-used defaults for a
-#' scandinavian locale.
-#'
-#' @param df The data to write.
-#' @param encoding The encoding to use when writing.
-#' @author Kristian D. Olsen
-#' @note This function only works on Windows, and the data-size cannot exceeed 128kb.
-#' @export
-#' @examples 
-#' x %>% write_clipboard()
-
-write_clipboard <- function(df, encoding = "latin1") {
-  
-  if (!identical(Sys.info()["sysname"], "Windows")) {
-    stop("Writing to clipboard requires Windows OS")
-  }
-  
-  if (object.size(df) > 120000) {
-    stop("The data is too large to write to clipboard", call. = FALSE)
-  }
-  
-  if (!inherits(df, "data.frame")) {
-    stop("The data must be a data.frame", call. = FALSE)
-  }
-  
-  write.table(x = df,
-              file = "clipboard-128",
-              sep = "\t",
-              na = "",
-              dec = ",",
-              row.names = FALSE,
-              fileEncoding = encoding)
-}
-
 # Output wrappers --------------------------------------------------------------
 
 write_xlsx <- function(lst, file) {
@@ -160,7 +160,7 @@ write_xlsx <- function(lst, file) {
   
   
   lapply(names(lst), function(nm, lst, wb) {
-    write_sheets(lst[[nm]], wb = wb, sheet = nm, append = FALSE)}, 
+    to_sheet(lst[[nm]], wb = wb, sheet = nm, append = FALSE)}, 
     lst, wb)
   
   openxlsx::saveWorkbook(wb, file, overwrite = TRUE)
