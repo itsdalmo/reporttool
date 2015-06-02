13be8c1b3ba7f796e98f5533fea3d8f590ff1da2
Kristian <kristian@doingit.no>
2015-06-02 23:20:02 +0200

    Sample

--- R/topline.R
+++ R/topline.R
@@ -5,16 +5,17 @@
 #' 
 #' @param df The data.frame containing the study data
 #' @param mainentity Name of the mainentity (q1) column in the data
-#' @param me_open The text column which contains the open response for 'other' in 
+#' @param entity_other The text column which contains the open response for 'other' in 
 #' mainentity.
 #' @param scores A vector with columnnames for variables you would like a mean from.
+#' @param sample The name of the column containing sample information
 #' @author Kristian D. Olsen
 #' @return A list
 #' @export
 #' @examples 
 #' x <- topline(df)
 
-topline <- function(df, mainentity = "q1", entity_other = "q1_open", scores = c("q3", "q6", "q16")) {
+topline <- function(df, mainentity = "q1", entity_other = "q1_open", scores = c("q3", "q6", "q16"), sample = "sample") {
   
   n <- nrow(df)
   names(df)[names(df) %in% mainentity] <- "entity"
@@ -27,7 +28,6 @@ topline <- function(df, mainentity = "q1", entity_other = "q1_open", scores = c(
   ents <- add_entities(df$entity)[c("entity", "n")]
   ents <- merge(ents, aggregate(df[scores], df["entity"], FUN = mean, na.rm = TRUE), by = "entity")
   
-  # Append the average
   ents_total <- data.frame("entity" = "Total", "n" = length(na.omit(df$entity)))
   ents_total[scores] <- apply(df[scores], 2, mean, na.rm = TRUE)
   
@@ -35,7 +35,19 @@ topline <- function(df, mainentity = "q1", entity_other = "q1_open", scores = c(
   
   # Clean NaN's
   ents[scores] <- apply(ents[scores], 2, function(x) ifelse(is.nan(x), NA, x))
-
+  
+  # Add sample counts
+  samp_total <- do.call(cbind, as.list(tapply(df$entity, df$sample, FUN = length)))
+  samp_total <- as.data.frame(samp_total, stringsAsFactors = FALSE)
+  samp_total$entity <- "Total"
+  
+  samp <- tapply(df$entity, df[c("entity", sample)], FUN = length)
+  samp <- cbind(data.frame("entity" = dimnames(samp)$entity, as.data.frame(samp, stringsAsFactors = FALSE)))
+  rownames(samp) <- NULL
+  
+  ents <- merge(rbind(samp, samp_total), ents, by = "entity")
+  names(ents) <- tolower(names(ents))
+  
   # Make a table for 'other' and sort it
   other <- as.data.frame(table(na.omit(df[entity_other])), stringsAsFactors = FALSE)
   names(other) <- c("name", "n")
