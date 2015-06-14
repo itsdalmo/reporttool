1184a77ad737b18df8394207cfc169bfaf129d96
itsdalmo <kristian@doingit.no>
2015-06-15 00:17:18 +0200

    Start testing prepare_data

--- tests/testthat/test-prepare_data.R
+++ tests/testthat/test-prepare_data.R
@@ -9,7 +9,7 @@ test_that("Prepare data (mean) from raw works" , {
   x <- prepare_data(raw, latents = "mean", impute = FALSE)
   
   expect_true(all(c("mainentity", cfg$latent_names) %in% x$mm$latent[1:8]))
-  expect_identical(c(x$mm$manifest, "w"), names(x$df))
+  expect_true(all(c("percent_missing", "w", cfg$latent_names) %in% names(x$df)))
   expect_identical(x$ents$entity, "Example")
   expect_identical(x$ents$n, 19L)
   
@@ -20,8 +20,8 @@ test_that("Prepare data (mean) from raw works" , {
   lst[["data"]] <- raw
   x <- prepare_data(lst, latents = "mean", impute = FALSE)
   
-  expect_true(all(c("mainentity", cfg$latent_names) %in% x$mm$latent[1:8]))
-  expect_identical(c(x$mm$manifest, "w"), names(x$df))
+  expect_true(all(c("mainentity", cfg$latent_names) %in% tolower(x$mm$latent)))
+  expect_true(all(c("percent_missing", "w", cfg$latent_names) %in% names(x$df)))
   expect_identical(x$ents$entity, "Example")
   expect_identical(x$ents$n, 19L)
   
