460a6b350693d29bf1b5f77510d774b4aeb6f1c8
Kristian <kristian@doingit.no>
2015-08-10 12:43:07 +0200

    as_factor recieved an optional drop_na argument

--- R/data_input.R
+++ R/data_input.R
@@ -124,7 +124,7 @@ read_spss <- function(file, codebook) {
   }
   
   # Convert labelled to factors
-  df <- lapply(df, function(x) { if (inherits(x, "labelled")) haven::as_factor(x) else x })
+  df <- lapply(df, function(x) { if (inherits(x, "labelled")) haven::as_factor(x, drop_na = FALSE) else x })
   df <- as.data.frame(df, stringsAsFactors = FALSE)
   
   # Use factor levels to populate values in mm
