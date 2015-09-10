4ce424e7edb55d279f50c17ddb6814a19a2c2a17
Kristian <kristian@doingit.no>
2015-09-10 15:24:23 +0200

    Removed option for plspm and imputation.

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -5,8 +5,10 @@
 #' for each entity. 
 #' 
 #' @param survey A survey object.
-#' @param type The approach to use for latents. Either \code{mean}, \code{pls}, 
-#' \code{pls-wizard} (no latents or EM-variables) or \code{none} (no latents).
+#' @param type The approach to use for latents. Either \code{mean}, \code{pls},
+#' or \code{none} (no latents). If \code{pls} is used, only \code{coderesp} and
+#' \code{percent_missing} are added to the data. I.e. it is a preparation for the
+#' PLS-wizard.
 #' @return Returns the survey with EM-variables and latent scores added using the
 #' specified method.
 #' @author Kristian D. Olsen
@@ -41,8 +43,8 @@ prepare_data <- function(survey, type = "mean", cutoff = .3) {
   }
   
   # Check type
-  if (!type %in% c("none", "mean", "pls", "pls-wizard")) {
-      stop("Invalid type. Please use 'none', 'mean', 'pls' or 'pls-wizard'.", call. = FALSE)
+  if (!type %in% c("none", "mean", "pls")) {
+      stop("Invalid type. Please use 'none', 'mean' or 'pls'.", call. = FALSE)
   } else {
     survey$cfg$value[survey$cfg$config %in% "latents"] <- type
   }
@@ -70,7 +72,7 @@ prepare_data <- function(survey, type = "mean", cutoff = .3) {
   
   survey$df <- cbind("coderesp" = 1:nrow(survey$df), survey$df)
   
-  # Clean and rescale scores{
+  # Clean and rescale scores
   survey$df[model$EM] <- lapply(survey$df[model$manifest], clean_score)
   survey$df[model$EM] <- lapply(survey$df[model$EM], rescale_score)
 
@@ -84,31 +86,11 @@ prepare_data <- function(survey, type = "mean", cutoff = .3) {
   
   survey$ents$valid <- entities$valid[match(survey$ents$entity, entities$entity)]
   survey$ents$valid[is.na(survey$ents$valid)] <- 0
-
-  # Impute missing
-  if (type == "pls") {
-    imputed <- impute_missing(survey$df, model$EM, cutoff)
-    
-    if (is.character(imputed)) {
-      warning(imputed, call. = FALSE)
-      imputed <- FALSE
-    } else {
-      survey$df[model$EM] <- imputed
-      imputed <- TRUE
-    }
-  }
   
   # Add latents to the data
   if (type == "mean") {
     survey <- latents_mean(survey, model, cutoff)
-  } 
-  
-  if (type == "pls" && imputed) {
-    survey <- latents_pls(survey, model, mainentity, cutoff)
-  }
-  
-  # If pls-wizard is meant to be used, remove EM-variables
-  if (type == "pls-wizard") {
+  } else if (type == "pls") {
     survey$df <- survey$df[!names(survey$df) %in% model$EM]
   }
   
@@ -138,103 +120,5 @@ latents_mean <- function(survey, model, cutoff) {
   
   # Return
   survey
-  
-}
-
-latents_pls <- function(survey, model, mainentity, cutoff) {
-  
-  # Set seed (reproducible) and subset data
-  set.seed(1000)
-  df <- survey$df[c(mainentity, model$EM, "percent_missing")]
-  
-  # Create identifier for a join after analysis
-  df$imp_id <- 1:nrow(df)
-  mod_data <- df[df$percent_missing <= cutoff, ]
-  
-  # Get latent names
-  manifests <- model$EM
-  latents <- default$latents
-  
-  model <- list("modes" = rep("A", length(latents)),
-                "inner" = default$model,
-                "outer" = lapply(latents, function(x, mm) {
-                  o <- mm$manifest[stri_trans_tolower(mm$latent) %in% x]
-                  stri_c(stri_trans_tolower(o), "em") }, model))
-  
-  names(model$outer) <- latents
-  
-  # Run the analysis for each entity
-  entities <- unique(mod_data[[mainentity]])
-  list_em_data <- lapply(entities, function(i, ent_var, df, model) {
-    
-    df <- df[df[[ent_var]] == i,]
-    em <- plspm::plspm(df, model$inner, model$outer, model$modes, scaled=FALSE, boot.val=TRUE)
-    
-    em_df <- cbind(df, plspm::rescale(em))
-    em_mm <- as.data.frame(em$path_coefs, stringsAsFactors = FALSE)
-    
-    em_mm <- lapply(Map('[', em_mm), function(x, nm) {
-      out <- setNames(x, nm)
-      out[x != 0L]
-    }, colnames(em_mm))
-    
-    em_mm <- data.frame(as.list(unlist(em_mm)), stringsAsFactors = FALSE)
-    em_mm$entity <- i
-    
-    # Return list
-    list("data" = em_df, "weights" = em_mm)
-    
-  }, mainentity, mod_data, model)
-  
-  mod_data <- do.call('rbind', lapply(list_em_data, '[[', 1))
-  mod_data <- mod_data[order(mod_data$imp_id), ]
-  
-  mod_coefs <- do.call('rbind', lapply(list_em_data, '[[', 2))
-  names(mod_coefs) <- gsub("\\.", "-", names(mod_coefs))
-  
-  # Join the estimated latents with the original dataset
-  input$df[latents] <- NA
-  input$df[df$imp_id %in% mod_data$imp_id, c(manifests, latents)] <- mod_data[c(manifests, latents)]
-  
-  # Add path coefficients to entities and return results
-  input$ents <- merge(input$ents, mod_coefs, by = "entity", all = TRUE)
-  
-  input
-  
-}
-
-impute_missing <- function(df, vars, cutoff) {
-  
-  # Set seed (reproducible)
-  set.seed(1000)
-  
-  # Subset the data.frame
-  df <- df[c(vars, "percent_missing")]
-  
-  # Create identifier for a join after analysis
-  df$imp_id <- 1:nrow(df)
-  
-  # Impute missing values
-  nvars <- ncol(df[vars])
-  bounds <- matrix(c(1:nvars, rep(0, nvars), rep(100, nvars)), ncol=3)
-  imp_data <- df[df$percent_missing <= cutoff, ]
-  
-  # Capture output due to print usage in Amelia-package
-  junk <- capture.output(
-    imp_data <- Amelia::amelia(imp_data, 5, bounds = bounds, boot.type="none", 
-                               idvars = c("percent_missing", "imp_id"))
-    )
-  
-  # Check message for errors (Not normal)
-  if (!stringi::stri_detect(imp_data$message, regex = "Normal")) {
-    return(stri_c("Failed to impute missing values:\n", imp_data$message))
-  } 
-  
-  # Get the imputed data and merge
-  imp_data <- imp_data$imputations$imp5
-  df[df$imp_id %in% imp_data$imp_id, vars] <- imp_data[vars]
-  
-  # Return
-  df[vars]
   
 }
