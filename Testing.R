b276691cfa4971f12253dcf4514a509e6c5e4596
Kristian <kristian@doingit.no>
2015-09-14 11:57:01 +0200

    convert_ is no longer needed with dplyr methods. Same with set_colnames.

---
+++ Testing.R
@@ -0,0 +1,20 @@
+library(devtools);test()
+library(stringi)
+
+x <- read_data("c:/Users/krist_000/Desktop/Data_SK_11 septemeber.sav")
+
+
+xx <- survey(x) %>%
+      rename(q1_org = Q1, q1 = b_4) %>%
+      mutate(q1 = stri_replace_all(var, "", regex = "\""), 
+             q1 = factor(q1, levels = unique(q1))) %>%
+      set_association(common = TRUE) %>% 
+      add_entities() %>%
+      set_config() %>%
+      set_translation() %>%
+      prepare_data()
+
+
+all(names(xx$df) %in% xx$mm$manifest)
+
+write_data(xx, "SK 11 september.xlsx")
