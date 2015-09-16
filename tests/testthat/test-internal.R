74f2153c8c89e38c862fe505a1f60f409b0d793f
dalmo@osx <kristian@doingit.no>
2015-09-16 20:50:43 +0200

    More tests

---
+++ tests/testthat/test-internal.R
@@ -0,0 +1,10 @@
+context("internal")
+
+raw <- read_data("test_raw.xlsx")
+srv <- survey(raw)
+
+test_that("selecting variables", {
+  
+  
+  
+})
---
+++ tests/testthat/test-prepare.R
@@ -0,0 +1,28 @@
+context("Prepare data")
+
+raw <- read_data("test_raw.xlsx")
+
+test_that("Prepare data", {
+  
+  x <- survey(raw)
+  x <- add_mm(x)
+  x <- set_association(x, common = TRUE)
+  x <- add_entities(x)
+  x <- set_config(x)
+  x <- set_translation(x)
+  
+  # Prepare data
+  y <- prepare_data(x, type = "mean")
+  z <- prepare_data(x, type = "pls")
+  
+  # Check results
+  expect_identical(names(y$df)[1], "coderesp")
+  expect_true(all(c(default$latents, "percent_missing", "q5aem") %in% names(y$df)))
+  expect_identical(names(y$df), y$mm$manifest)
+  
+  # PLS
+  expect_identical(names(z$df)[1], "coderesp")
+  expect_identical(names(z$df)[ncol(z$df)], "percent_missing")
+  expect_true(!any(default$latents) %in% names(z$df))
+  
+})
