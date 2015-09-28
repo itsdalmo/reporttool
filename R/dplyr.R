4a9eaa5404715c89e546f4be617b8238b8bc2d54
Kristian <kristian@doingit.no>
2015-09-28 14:38:17 +0200

    Don't attempt to update entities if nrow == 0L

--- R/dplyr.R
+++ R/dplyr.R
@@ -113,7 +113,7 @@ filter_.survey <- function(srv, ..., .dots) {
   
   # Update entities if the association is set and rows have been dropped
   has_me <- any(stri_detect(srv$mm$latent, regex = "mainentity"), na.rm = TRUE)
-  if (has_me && nrow(srv$df) < n) {
+  if (has_me && nrow(srv$df) < n && nrow(srv$df) > 0L) {
     srv <- add_entities(srv)
   } else {
     warning("Entities could not be updated.", call. = FALSE)
