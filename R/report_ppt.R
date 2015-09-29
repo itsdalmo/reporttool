18d19cac38e597d0498420a9a46ccc191f31936d
Kristian <kristian@doingit.no>
2015-09-29 12:04:01 +0200

    Updated report generation with PPT

--- R/report_ppt.R
+++ R/report_ppt.R
@@ -16,7 +16,12 @@
 #' x <- survey(data.frame("test" = 1, stringsAsFactors = FALSE))
 #' x %>% add_mm()
 
-generate_ppt <- function(entity, rmd, dir, envir) {
+generate_ppt <- function(entity, rmd, dir, envir, font = "Sisco Book") {
+  
+  # Set default font for ReporteRs
+  ofont <- getOption("ReporteRs-default-font")
+  options("ReporteRs-default-font" = font)
+  on.exit(options("ReporteRs-default-font" = ofont), add = TRUE)
   
   # Copy theme if it does not exist
   dir.create(file.path(dir, "PPT"), showWarnings = FALSE)
@@ -25,9 +30,16 @@ generate_ppt <- function(entity, rmd, dir, envir) {
   # Evaluate the markdown
   res <- evaluate_rmd(rmd, envir = envir)
   
-  # Create report
+  # Create report and add the first slide
   doc <- ReporteRs::pptx(template = file.path(dir, "PPT", "ppt_template.pptx"))
-  doc <- to_ppt(doc, res)
+  doc <- ReporteRs::addSlide(doc, slide.layout = "titleslide")
+  doc <- ReporteRs::addTitle(doc, value = res$title)
+  doc <- ReporteRs::addParagraph(doc, value = res$date)
+  doc <- ReporteRs::addParagraph(doc, value = res$author)
+  doc <- ReporteRs::addParagraph(doc, value = res$subtitle)
+  
+  # Add title etc
+  doc <- to_ppt(doc, res$code)
   
   ReporteRs::writeDoc(doc, file = stri_c(file.path(dir, "PPT", stri_c(entity, ".pptx"))))
   
@@ -39,38 +51,20 @@ generate_ppt <- function(entity, rmd, dir, envir) {
 to_ppt <- function(doc, res) {
   
   # Get the types of data in our results
-  type <- vapply(res, function(x) {
-    
-    if (length(x) == 0L || is.na(x)) return("unknown")
-    
-    if (is.character(x)) {
-      if (stri_detect(x, regex = "^#[^#].*")) {
-        "title"
-      } else if (stri_detect(x, regex = "^##[^#].*")) {
-        "subtitle"
-      } else if (stri_detect(x, regex = "begin\\{table\\}")) {
-        "table"
-      } else {
-        "markdown"
-      }
-    } else if (inherits(x, "recordedplot")) {
-      "recordedplot"
-    } else {
-      "other"
-    }}, character(1))
+  type <- result_types(res)
     
   # Get the actual results
   title <- " "; subtitle <- " "
   
   for (i in seq_along(res)) {
     if (type[i] == "title") {
-      title <- stri_replace(res[[i]], "", regex = "^#[^#]\\s*")
+      title <- stri_replace(res[[i]], "", regex = "^#[^#]")
     } else if (type[i] == "subtitle") {
-      subtitle <- stri_replace(res[[i]], "", regex = "^##[^#]\\s*")
+      subtitle <- stri_replace(res[[i]], "", regex = "^##[^#]")
     } else if (type[i] == "table") {
       doc <- ReporteRs::addSlide(doc, slide.layout = 'tableslide')
       doc <- ReporteRs::addTitle(doc, title)
-      # doc <- ReporteRs::addFlexTable(doc, res[[i]])
+      doc <- ReporteRs::addFlexTable(doc, format_flextable(res[[i]]))
       doc <- ReporteRs::addParagraph(doc, subtitle)
     } else if (type[i] == "markdown") {
       doc <- ReporteRs::addSlide(doc, slide.layout = 'standardslide')
@@ -82,11 +76,45 @@ to_ppt <- function(doc, res) {
       doc <- ReporteRs::addTitle(doc, title)
       doc <- ReporteRs::addPlot(doc, fun = print, x = res[[i]])
       doc <- ReporteRs::addParagraph(doc, subtitle)
-    } else if (type[i] == "other") {
+    } else if (type[i] == "footnote") {
+      warning("Footnotes are not supported and will not be included.", call. = FALSE)
+    } else if (type[i] == "latextable") {
+      warning("Latex tables are not supported and will not be included.", call. = FALSE)
     }
+    
   }
 
   return(doc)
+  
+}
+
+result_types <- function(res) {
+  
+  vapply(res, function(x) {
+    
+    if (length(x) == 0L || is.na(x)) return("unknown")
+    
+    if (is.character(x)) {
+      if (stri_detect(x, regex = "^#[^#].*")) {
+        "title" 
+      } else if (stri_detect(x, regex = "^##[^#].*")) {
+        "subtitle"
+      } else if (stri_detect(x, regex = "begin\\{table\\}")) {
+        "latextable"
+      } else if (stri_detect(x, regex = "footnoteextra\\{")) {
+        "footnote"
+      } else {
+        "markdown"
+      }
+    } else if (inherits(x, "recordedplot")) {
+      "recordedplot"
+    } else if (inherits(x, "source")) {
+      "source"
+    } else if (inherits(x, "data.frame")) {
+      "table"
+    } else {
+      "unknown"
+    }}, character(1))
   
 }
 
