9c4ff829d2372bfe2c105b6784bc905feb1bd242
Kristian <kristian@doingit.no>
2015-09-29 17:06:46 +0200

    Testtest

--- Testing.R
+++ Testing.R
@@ -1,57 +1,48 @@
 library(devtools); test()
 library(stringi)
 
+srv <- prepare_survey(srv)
+entities <- levels(srv$df$mainentity)[1]
+rmd <- "c:/Github/EPSI/Kontorstudien PM 2015/Kontorstudien PM 2015.Rmd"
+options("ReporteRs-default-font" = "Arial")
 
-# osrv from a report
-rmd <- "../EPSI/Lyse ladestasjoner/Lyse ladestasjoner.Rmd"
-md <- rmd_to_r(rmd, write = FALSE)
+report = rmd
+entity = entities
+type = "ppt"
+font = "Arial"
 
+# generate_report(report = rmd, entity = entities, type = "ppt")
+report <- clean_path(report)
+dir <- dirname(report)
+srv_envir <- list2env(srv, parent = environment())
 
-srv <- osrv
-srv$entity <- "Lyse ladestasjoner"
-envir <- list2env(srv)
-
-md <- readLines(rmd, encoding = "UTF-8")
-md <- stringi::stri_replace(md, format(Sys.Date(), "%Y"), regex = "REPLACE_DATE")
-
-
-# ------------------------------------------------------------------------------
-
-ev <- evaluate_rmd(md, envir = envir)
-test <- ev
-ev <- test$code
-
-# ev[sapply(ev, class) == "recordedplot"]
-ev[sapply(ev, class) == "character"]
-
-# ------------------------------------------------------------------------------
+md <- readLines(report, encoding = "UTF-8")
+md <- stringi::stri_replace_all(md, format(Sys.Date(), "%Y"), regex = "REPLACE_DATE")
+dir.create(file.path(dir, "PPT"), showWarnings = FALSE)
+copy_ppt_theme(file.path(dir, "PPT"))
+lapply(entity, generate_rmd, md, dir)
 
 
-rmd <- rmd_to_r(rmd, write = FALSE)
-
-srv %>% generate_report("C:/Github/EPSI/Lyse ladestasjoner/Lyse ladestasjoner.Rmd", type = "ppt")
-
-
-rmd <- md
+# lapply(entity, generate_ppt, dir, srv_envir)
+rmd <- file.path(dir, "Markdown", stri_c(entity, ".Rmd"))
+rmd <- readLines(rmd, encoding = "UTF-8")
 
 if (!any(stri_detect(rmd, regex = "##\\+"), na.rm = TRUE)) {
   rmd <- rmd_to_r(rmd, write = FALSE)
 }
 
 # Separate out YAML
+envir = srv_envir
+
 is_yaml <- which(stri_detect(rmd, regex = default$pattern$code$yaml))
-if (!length(is_yaml)) stop("Could not find the YAML frontmatter.", call. = FALSE)
-yaml <- extract_yaml(rmd)
-rmd <- rmd[-c(is_yaml[1]:is_yaml[2])]
-
-res <- evaluate_rmd(rmd, envir = envir)
-
-
-
-
-
-rmd <- rmd[(length(rmd)-132):length(rmd)]
-
+if (!length(is_yaml)) {
+  stop("Could not find the YAML frontmatter.", call. = FALSE)
+} else {
+  yaml <- extract_yaml(rmd)
+  rmd <- rmd[-c(is_yaml[1]:is_yaml[2])]
+}
 
 # Evaluate the markdown
 res <- evaluate_rmd(rmd, envir = envir)
+
+res[sapply(res, class) == "recordedplot"]
