f3cafa623a98d1184ecd04613f9484eda564f2a3
Kristian <kristian@doingit.no>
2015-10-01 18:08:48 +0200

    Functions to get settings and translations.

---
+++ R/get_.R
@@ -0,0 +1,23 @@
+get_translation <- function(srv, tr) {
+  
+  value <- filter(srv$tr, original %in% stri_trans_tolower(tr))[["replacement"]]
+  if (!length(value) || is.na(value)) {
+    stop(stri_c("\'", tr, "\'"), " was not found in translations.", call. = FALSE)
+  } 
+  
+  value
+  
+}
+
+get_config <- function(srv, cfg) {
+  
+  value <- filter(srv$cfg, config %in% stri_trans_tolower(cfg))[["value"]]
+  if (!length(value) || is.na(value)) {
+    stop(stri_c("\'", cfg, "\'"), " was not found in config.", call. = FALSE)
+  } 
+  
+  value
+  
+}
+
+get_data <- function(srv) srv$df
