50590ce1ce10a7ce45227dc872e6bf9a4a9450c7
Kristian <kristian@doingit.no>
2015-10-08 17:44:48 +0200

    Changes: Return early if only one variable is specified, and it is also the mainentity in measurement model. Allow filtering of percent_missing to be skipped, and add netural weights (w) if it is not present in the data.

--- R/survey_table.R
+++ R/survey_table.R
@@ -16,9 +16,10 @@
 #'    weight the results for the average. This also means that the function requires
 #'    that the weight column (\code{w}) is present in the data.}  
 #' 
-#'    \item{\code{filter}}{Always filters missing values \code{NA}
+#'    \item{\code{filter}}{Filters missing values \code{NA}
 #'    for all groups and the variables that go into the table, as well as 
-#'    \code{percent_missing} which exceeds the cutoff in the surveys config.}
+#'    \code{percent_missing} which exceeds the cutoff in the surveys config. Setting
+#'    \code{filter_missing} to \code{FALSE} overrides the filtering of \code{percent_missing}.}
 #'
 #'    \item{\code{count}}{For both numeric and factor variables, this function always
 #'    provides a count for the number of valid observations (after filtering) in each group.}
@@ -43,6 +44,8 @@
 #' @param weight When \code{TRUE}, the average will be weighted (assumes weights are in column 'w').
 #' @param question When \code{TRUE}, the question text specified in the measurement
 #' model will be included in the table (provided they are not empty strings). 
+#' @param filter_missing Set to \code{FALSE} to NOT remove observations with \code{percent_missing}
+#' above the cutoff that is set in config.
 #' @param contrast Set to \code{FALSE} if a contrast exist but you want to use the study
 #' average instead.
 #' @author Kristian D. Olsen
@@ -50,14 +53,14 @@
 #' @examples 
 #' x %>% group_by(q7_service) %>% survey_table(image:loyal)
 
-survey_table <- function(srv, ..., wide = TRUE, weight = TRUE, question = TRUE, contrast = TRUE) {
+survey_table <- function(srv, ..., wide = TRUE, weight = TRUE, question = TRUE, filter_missing = TRUE, contrast = TRUE) {
   dots <- lazyeval::lazy_dots(...)
-  survey_table_(srv, .dots = dots, wide = wide, weight = weight, question = question, contrast = contrast)
+  survey_table_(srv, dots = dots, wide = wide, weight = weight, question = question, contrast = contrast)
 }
 
 #' @export
 #' @rdname survey_table
-survey_table_ <- function(srv, ..., .dots, wide = TRUE, weight = TRUE, question = TRUE, contrast = TRUE) {
+survey_table_ <- function(srv, ..., dots, wide = TRUE, weight = TRUE, question = TRUE, filter_missing = TRUE, contrast = TRUE) {
   
   # Check the input
   if (!is.survey(srv)) {
@@ -67,29 +70,39 @@ survey_table_ <- function(srv, ..., .dots, wide = TRUE, weight = TRUE, question 
     cutoff <- as.numeric(get_config(srv, "cutoff"))
   }
   
-  dots <- lazyeval::all_dots(.dots, ...)
+  dots <- lazyeval::all_dots(dots, ...)
   if(!length(dots)) stop("No variables specified.", call. = FALSE)
   
   # Extract groups and ungroup
   grouping <- as.character(groups(srv))
   if (is.null(grouping) || !length(grouping)) {
     grouping <- mainentity
-    warning("No preexisting groups. Grouping by '", mainentity, "'.", call. = FALSE)
+    warning("No preexisting groups. Grouping by '", mainentity, "' (mainentity).", call. = FALSE)
   } else {
     mainentity <- grouping[1]
     srv <- ungroup(srv)
   }
-
-  # Filter missing and subset columns
-  srv <- suppressWarnings(filter_(srv, .dots = lazyeval::lazy_dots(percent_missing <= cutoff)))
-  srv <- select_(srv, .dots = c(grouping, "w", dots))
   
+  # Filter missing and add weights if not found
+  if (filter_missing) {
+    mt <- lazyeval::lazy_dots(percent_missing <= cutoff)
+    srv <- suppressWarnings(filter_(srv, .dots = mt))
+  }
+  
+  if (!"w" %in% names(srv$df)) {
+    srv <- mutate(srv, w = 1L)
+  }
+  
+  # Subset columns
+  srv <- suppressWarnings(select_(srv, .dots = c(grouping, "w", dots)))
+
   # Get the grouping variable
   entities <- srv$df[[mainentity]]
   entities <- if (is.factor(entities)) levels(entities) else unique(as.character(entities))
-  
+
   # 2x length dataset to produce average as well
   vars <- select_vars_(names(srv$df), c(grouping, dots))
+
   if (contrast && is.data.frame(srv$cd) && nrow(srv$cd) && all(vars %in% names(srv$cd))) {
     tr <- get_translation(srv, "contrast_average")
     mt <- lazyeval::interp(~x, x = tr)
@@ -99,11 +112,11 @@ survey_table_ <- function(srv, ..., .dots, wide = TRUE, weight = TRUE, question 
     mt <- lazyeval::interp(~x, x = tr)
     df <- bind_rows(srv$df, mutate_(srv$df, .dots = setNames(list(mt), mainentity)))
   }
-  
+
   # Mainentity should be a (ordered) factor variable
   mt <- lazyeval::interp(~factor(x, levels = y, ordered = TRUE), x = as.name(mainentity), y = c(entities, tr))
   df <- mutate_(df, .dots = setNames(list(mt), mainentity))
-  
+
   # Set w to 1 for all rows but the average
   if (weight) {
     df <- mutate_(df, .dots = lazyeval::lazy_dots(w = as.numeric(w)))
@@ -120,13 +133,18 @@ survey_table_ <- function(srv, ..., .dots, wide = TRUE, weight = TRUE, question 
     warning("The following columns are character vectors and will not be included:\n",
             stri_c(is_character, collapse = ", "), call. = FALSE)
   }
-  
+
   # Either factor or numeric
   vars <- select_vars_(names(df), args = dots)
 
+  if (identical(unname(vars), mainentity)) {
+    # Return early if only mainentity is specified
+    return(count_(df, vars = vars, wt = quote(w)))
+  }
+
   is_factor <- all(vapply(df[vars], is.factor, logical(1)))
   is_numeric <- all(vapply(df[vars], is.numeric, logical(1)))
-  
+
   if (!all(is_factor) && !all(is_numeric)) {
     stop("All selected columns must either be factor or numeric (no mixing).", call. = FALSE)
   } else if (all(is_factor)) {
@@ -159,11 +177,12 @@ survey_table_ <- function(srv, ..., .dots, wide = TRUE, weight = TRUE, question 
 
   # Join questions if desired
   if (question) {
+    srv <- use_latent_translation(srv)
     var_names <- filter(select(srv$mm, manifest, question), manifest %in% df_count$manifest, !question %in% c("", " "))
     missing <- setdiff(vars, var_names$manifest)
     if (length(missing)) {
-      message("The following variables had empty 'questions' and will not been replaced:\n",
-              stri_c(missing, collapse = ", "))
+      warning("The following variables had empty 'questions' and have not been replaced:\n",
+              stri_c(missing, collapse = ", "), call. = FALSE)
     }
     df_count <- suppressWarnings(left_join(df_count, select(srv$mm, manifest, question), by = c("manifest" = "manifest")))
     df_count <- select_(df_count, .dots = c(by_group, "question", if (is_factor) "answer" else NULL, "n"))
@@ -174,12 +193,12 @@ survey_table_ <- function(srv, ..., .dots, wide = TRUE, weight = TRUE, question 
   if (is_numeric) {
     df <- group_by_(df, .dots = by_group)
     df <- summarise_each_(df, funs(weighted.mean(., w = w, na.rm = TRUE)), vars = "answer")
-    df <- suppressMessages(left_join(df_count, df))
+    df <- suppressWarnings(suppressMessages(left_join(df_count, df)))
   } else if (is_factor) {
     df <- count_(df, vars = c(by_group), wt = quote(w))
     df <- mutate_(df, .dots = lazyeval::lazy_dots(proportion = prop.table(n), n = sum(n)))
     df <- select_(df, .dots = lazyeval::lazy_dots(-n))
-    df <- suppressMessages(left_join(df_count, df))
+    df <- suppressWarnings(suppressMessages(left_join(df_count, df)))
     df <- tidyr::replace_na(df, replace = list("proportion" = 0))
     
     # Patchwork
